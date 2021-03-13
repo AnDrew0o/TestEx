fe9aeef2b23a747938619ece8eb45dcd7c5aa7a0
AnDrew0o <57329824+AnDrew0o@users.noreply.github.com>
2021-03-14 00:03:37 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import csv
 
 date_list = []
-skip_count = 0
+name_list = []
 
 old_file_name = "acme_worksheet.csv"
 new_file_name = "acme_worksheet_new.csv"
@@ -9,34 +9,22 @@ new_file_name = "acme_worksheet_new.csv"
 with open(old_file_name, "r") as old_file:
     file_reader = csv.DictReader(old_file)
     for row in file_reader:
-        if row["Date"] in date_list:
-            skip_count += 1
-        else:
+        if row["Date"] not in date_list:
             date_list.append(row["Date"])
+        if row["Employee Name"] not in name_list:
+            name_list.append(row["Employee Name"])
 
 with open(new_file_name, "w", newline="") as new_file:
     file_write = new_file.write("Name/Date, ")
     file_writer = csv.writer(new_file)
     file_writer.writerow(date_list)
 
-name_list = []
-
-with open(old_file_name,"r") as old_file:
-    file_reader = csv.DictReader(old_file)
-    for row in file_reader:
-        if row["Employee Name"] in name_list:
-            skip_count += 1
-        else:
-            name_list.append(row["Employee Name"])
-
 time_dict = {}
-count = 0
-
 time_dict["Name/Date"] = 0
-for i in range(len(date_list)):
-    time_dict[date_list[i]] = 0
 
 for count in range(len(name_list)):
+    for i in range(len(date_list)):
+        time_dict[date_list[i]] = 0
     with open(old_file_name, "r") as old_file:
         file_reader = csv.DictReader(old_file)
         for row in file_reader:
@@ -46,5 +34,3 @@ for count in range(len(name_list)):
     with open(new_file_name, "a", newline="") as new_file:
         file_writer = csv.DictWriter(new_file, fieldnames=time_dict)
         file_writer.writerow(time_dict)
-    for i in range(len(date_list)):
-        time_dict[date_list[i]] = 0
