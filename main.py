a3bd99e755d0c1397be58bae4d1fe373c2e25b2f
AnDrew0o <dr.dron.234@gmail.com>
2021-03-13 21:05:01 +0200

    third commit

--- main.py
+++ main.py
@@ -57,19 +57,31 @@ with open("acme_worksheet.csv","r") as old_file:
             skip_count += 1
         else:
             name_list.append(row["Employee Name"])
-            skip_count += 1
 
 time_dict = {}
-print(name_list)
 count = 0
+'''
 #while count < len(name_list):
 with open("acme_worksheet.csv", "r") as old_file:
     file_reader = csv.DictReader(old_file)
     for row in file_reader:
         if row["Employee Name"] == name_list[count]:
             time_dict[row["Date"]] = row["Work Hours"]
+with open("acme_worksheet_new.csv", "a", newline="") as new_file:
+    new_file.write(row["Employee Name"] + ",")
+    file_writer = csv.DictWriter(new_file, fieldnames=date_list)
+    file_writer.writerow(time_dict)
+count +=1'''
+
+while count < len(name_list):
+    with open("acme_worksheet.csv", "r") as old_file:
+        file_reader = csv.DictReader(old_file)
+        for row in file_reader:
+            if row["Employee Name"] == name_list[count]:
+                time_dict["Name/Date"] = row["Employee Name"]
+                time_dict[row["Date"]] = row["Work Hours"]
+        print(time_dict, count)
     with open("acme_worksheet_new.csv", "a", newline="") as new_file:
-        new_file.write(row["Employee Name"] + ",")
-        file_writer = csv.DictWriter(new_file, fieldnames=date_list)
+        file_writer = csv.DictWriter(new_file, fieldnames=time_dict)
         file_writer.writerow(time_dict)
-count +=1
+    count += 1
