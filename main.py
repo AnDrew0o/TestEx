b9df67e80ead50924b40b1f3c3c6399eed550054
AnDrew0o <dr.dron.234@gmail.com>
2021-03-13 22:34:30 +0200

    5th commit

--- main.py
+++ main.py
@@ -26,13 +26,19 @@ with open("acme_worksheet.csv","r") as old_file:
         else:
             name_list.append(row["Employee Name"])
 
-time_dict = {}
+time_dict = {
+        'Name/Date':0,'Jun 29 2020':0, 'Jun 30 2020':0, 'Jul 01 2020':0, 'Jul 02 2020':0, 'Jul 03 2020':0, 'Jul 04 2020':0, 'Jul 05 2020':0
+    }
 count = 0
 
-while count < len(name_list):
+
+for count in range(len(name_list)):
     with open("acme_worksheet.csv", "r") as old_file:
         file_reader = csv.DictReader(old_file)
         for row in file_reader:
+            # if count == 0:
+            #     time_dict["Name/Date"] = 0
+            #     time_dict[row["Date"]] = 0
             if row["Employee Name"] == name_list[count]:
                 time_dict["Name/Date"] = row["Employee Name"]
                 time_dict[row["Date"]] = row["Work Hours"]
@@ -40,4 +46,8 @@ while count < len(name_list):
     with open("acme_worksheet_new.csv", "a", newline="") as new_file:
         file_writer = csv.DictWriter(new_file, fieldnames=time_dict)
         file_writer.writerow(time_dict)
-    count += 1
+    time_dict = {
+        'Name/Date':0,'Jun 29 2020':0, 'Jun 30 2020':0, 'Jul 01 2020':0, 'Jul 02 2020':0, 'Jul 03 2020':0, 'Jul 04 2020':0, 'Jul 05 2020':0
+    }
+
+print(date_list)
